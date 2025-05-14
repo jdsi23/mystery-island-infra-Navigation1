506685597fb8f52a99c83677bc6a1acd628d22c0
jdsi23 <jameslusk535@gmail.com>
2025-05-14 22:03:54 +0000

    PLS

--- navigation-app/app.py
+++ navigation-app/app.py
@@ -44,12 +44,11 @@ def home():
     <body style='text-align:center;'>
         <h1>🗺️ Welcome to Mystery Island</h1>
         <div class="map-container">
-           <img src='https://mystery-island-bucket-1bb6a92a.s3.amazonaws.com/map.png' alt='Mystery Island Map' style='width:90%;'>
-            <img src='https://mystery-island-bucket-1bb6a92a.s3.amazonaws.com/icon_volcano.png' class='icon' style='top:70px; left:120px;' onclick="getDirections('volcano')">
-            <img src='https://mystery-island-bucket-1bb6a92a.s3.amazonaws.com/icon_maze.png' class='icon' style='top:220px; left:60px;' onclick="getDirections('maze')">
-            <img src='https://mystery-island-bucket-1bb6a92a.s3.amazonaws.com/icon_boat.png' class='icon' style='top:400px; left:220px;' onclick="getDirections('boat')">
-            <img src='https://mystery-island-bucket-1bb6a92a.s3.amazonaws.com/icon_resort.png' class='icon' style='top:100px; left:320px;' onclick="getDirections('resort')">
-
+            <img src='https://i.postimg.cc/7hTkGYNG/map.png' alt='Mystery Island Map' style='width:90%;'>
+            <img src='https://i.postimg.cc/HswLBBQJ/icon-volcano.png' class='icon' style='top:70px; left:120px;' onclick="getDirections('volcano')">
+            <img src='https://i.postimg.cc/nr8H1DrY/icon-maze.png' class='icon' style='top:220px; left:60px;' onclick="getDirections('maze')">
+            <img src='https://i.postimg.cc/J7B0B105/icon-boat.png' class='icon' style='top:400px; left:220px;' onclick="getDirections('boat')">
+            <img src='https://i.postimg.cc/QdNjSs8P/icon-resort.png' class='icon' style='top:100px; left:320px;' onclick="getDirections('resort')">
         </div>
         <div id="directions"></div>
     </body>
