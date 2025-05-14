a55023af4f1deb9ab0902255350ef91ec1c9cd26
jdsi23 <jameslusk535@gmail.com>
2025-05-14 18:33:03 +0000

    PLS

--- navigation-app/start.sh
+++ navigation-app/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start Nginx in the background
-nginx
+nginx -g 'daemon off;' &  # force background mode
 
 # Start Flask app in the foreground (keeps the container alive)
 python3 app.py
